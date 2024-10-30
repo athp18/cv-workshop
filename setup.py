e08add19c08cd09d10642a2db99f521d610f1acf
athp18 <129908089+athp18@users.noreply.github.com>
2024-10-30 09:03:02 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='image_subplots',
+    version='0.1',
+    description='utility function',
+    author='Atharv Panditrao',
+    author_email='atharvpan@g.ucla.edu',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
